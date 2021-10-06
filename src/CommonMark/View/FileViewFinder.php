dab8a179a48ea3f4bc4e7fdc21319e4d55174af7
alfonsobries <alfonsobries@gmail.com>
2021-10-06 11:05:40 -0500

    Move the logic for numeric values to a custom method

--- src/CommonMark/View/FileViewFinder.php
+++ src/CommonMark/View/FileViewFinder.php
@@ -5,28 +5,57 @@ declare(strict_types=1);
 namespace ARKEcosystem\Foundation\CommonMark\View;
 
 use Illuminate\View\FileViewFinder as Finder;
+use InvalidArgumentException;
 use Spatie\Regex\Regex;
 
 final class FileViewFinder extends Finder
 {
     /**
-     * Get an array of possible view files.
+     * Find the given view in the list of paths.
      *
-     * @param string $name
+     * @param  string  $name
+     * @param  array  $paths
+     * @return string
      *
-     * @return array
+     * @throws \InvalidArgumentException
      */
-    protected function getPossibleViewFiles($name)
+    protected function findInPaths($name, $paths)
     {
-        return array_map(function ($extension) use ($name) : string {
-            // Match number with optional decimals
-            $regex = Regex::match('/^\d*(\.\d*)?$/', $name);
+        // Match number with optional decimals
+        $regex = Regex::match('/^\d*(\.\d*)?$/', $name);
+        $isNumericName = $regex->hasMatch();
 
-            if ($regex->hasMatch()) {
-                $name = rtrim(explode($regex->result(), $name)[0], '.');
+        foreach ((array) $paths as $path) {
+            if ($isNumericName) {
+                $possibleViewFiles = $this->getPossibleViewFilesForNumericName($name, $path);
+            } else {
+                $possibleViewFiles = $this->getPossibleViewFiles($name);
+            }
 
+            foreach ($possibleViewFiles as $file) {
+                if ($this->files->exists($viewPath = $path.'/'.$file)) {
+                    return $viewPath;
+                }
+            }
+        }
 
-                return str_replace('.', '/', $name).'/'.$regex->result().'.'.$extension;
+        throw new InvalidArgumentException("View [{$name}] not found.");
+    }
+
+    protected function getPossibleViewFilesForNumericName(string $name, string $path): array
+    {
+        $regex = Regex::match('/^\d*(\.\d*)?$/', $name);
+
+        return array_map(function ($extension) use ($path, $name, $regex) : string {
+            $name = rtrim(explode($regex->result(), $name)[0], '.');
+
+            $file =  str_replace('.', '/', $name).'/'.$regex->result().'.'.$extension;
+
+            // Only return the file if it exists, that prevents applying this
+            // custom logic to numbers returned from custom render functions
+            // like `src/UserInterface/Components/Number.php`
+            if ($this->files->exists($path.'/'.$file)) {
+                return $file;
             }
 
             return str_replace('.', '/', $name).'.'.$extension;
