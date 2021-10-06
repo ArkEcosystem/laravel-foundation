59050a3e24f1c79fb4925fb789c0a8eed92c0f3a
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-10-06 14:04:01 +0100

    update methods to match nodem's

--- src/UserInterface/Eloquent/Concerns/HasSchemalessAttributes.php
+++ src/UserInterface/Eloquent/Concerns/HasSchemalessAttributes.php
@@ -9,44 +9,94 @@ use Illuminate\Database\Eloquent\Model;
 
 trait HasSchemalessAttributes
 {
+    /**
+     * Scope the query to include the meta attributes.
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
     public function scopeWithExtraAttributes(): Builder
     {
         return $this->extra_attributes->modelScope();
     }
 
-    public function getMetaAttribute($name, $default = null)
+    /**
+     * Get the meta attribute for the model.
+     *
+     * @param string|array|null $name
+     * @param mixed             $default
+     *
+     * @return mixed
+     */
+    public function getMetaAttribute(string | array | null $name, $default = null)
     {
         return $this->extra_attributes->get($name, $default);
     }
 
-    public function setMetaAttribute($name, $value)
+    /**
+     * Set the meta attribute for the model.
+     *
+     * @param string|array $name
+     * @param mixed        $value
+     *
+     * @return self
+     */
+    public function setMetaAttribute(string | array $name, $value): self
     {
         $this->extra_attributes->set($name, $value);
 
-        $this->save();
+        return tap($this)->save();
     }
 
-    public function hasMetaAttribute($name)
+    /**
+     * Determine whether the model contains a given meta attribute.
+     *
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function hasMetaAttribute(string $name): bool
     {
-        return ! empty($this->extra_attributes->get($name));
+        return $this->extra_attributes->get($name) !== null;
     }
 
-    public function forgetMetaAttribute($name)
+    /**
+     * Remove the meta attribute for the model.
+     *
+     * @param string $name
+     *
+     * @return self
+     */
+    public function forgetMetaAttribute(string $name): self
     {
         $this->extra_attributes->forget($name);
 
-        $this->save();
+        return tap($this)->save();
     }
 
-    public function fillMetaAttributes($attributes)
+    /**
+     * Fill the model's meta attributes.
+     *
+     * @param array $attributes
+     *
+     * @return self
+     */
+    public function fillMetaAttributes(array $attributes): self
     {
         foreach ($attributes as $name => $value) {
             $this->extra_attributes->set($name, $value);
         }
 
-        $this->save();
+        return tap($this)->save();
     }
 
+    /**
+     * Update or create model with the schemaless attributes.
+     *
+     * @param array $attributes
+     * @param array $values
+     *
+     * @return self
+     */
     public static function updateOrCreateWithMeta(array $attributes, array $values): Model
     {
         $model = static::withExtraAttributes($attributes)->first();
