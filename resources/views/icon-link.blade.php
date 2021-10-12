f9c4b23c99a1e08c2e212cbed4a8305afb085e91
Maurizio <maurizio.lepora@gmail.com>
2021-10-12 08:44:20 +0200

    fix icon-link.blade.php

--- resources/views/icon-link.blade.php
+++ resources/views/icon-link.blade.php
@@ -1,4 +1,4 @@
 <a href="{{ $href }}" target="{{ $target ?? '_self' }}" rel="{{ $rel ?? ''}}" class="flex items-center space-x-2 font-semibold link">
-    @if(! ($hideIcon ?? false))<span><x-ark-icon name="link" /></span>@endif
+    @if(! ($hideIcon ?? false))<span><x-ark-icon name="link" size="sm" /></span>@endif
     <span>{{ $slot }}</span>
 </a>
