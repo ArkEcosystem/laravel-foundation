8646115171561e08bd9b8a1da99dcf9a9b4e15a1
Brian Faust <hello@basecode.sh>
2021-10-19 04:54:16 +0300

    wip

---
+++ src/Rules/ServiceLink.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ARKEcosystem\Foundation\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Str;
+use InvalidArgumentException;
+
+final class ServiceLink implements Rule
+{
+    public function __construct(private string $service)
+    {
+        //
+    }
+
+    public function passes($attribute, $value): bool
+    {
+        if ($this->service === 'hive') {
+            $regexes =  [
+                RegEx::socialMediaLink('hive'),
+                RegEx::socialMediaLink('ecency'),
+                RegEx::socialMediaLink('peakd'),
+            ];
+
+            return collect($regexes)->some(fn ($regex) => boolval(preg_match($regex, $value)));
+        }
+
+        if ($this->service === 'website') {
+            $expression = RegEx::get($this->service);
+        } elseif (array_key_exists($this->service, config('rules.regex.social_media_link'))) {
+            $expression = RegEx::socialMediaLink($this->service);
+        } elseif (array_key_exists($this->service, config('rules.regex.source_providers'))) {
+            $expression = RegEx::sourceProviderLink($this->service);
+        } else {
+            throw new InvalidArgumentException("Service [{$this->service}] has no regular expression.");
+        }
+
+        return boolval(preg_match($expression, $value));
+    }
+
+    public function message()
+    {
+        return trans('ui::validation.custom.'.Str::snake($this->service).'_url');
+    }
+}
---
+++ tests/Rules/InstagramUrlTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use ARKEcosystem\Foundation\Rules\ServiceLink;
+
+beforeEach(function (): void {
+    $this->subject = new ServiceLink('instagram');
+});
+
+it('validates a valid url', function ($url) {
+    $this->assertTrue($this->subject->passes('instagram_url', $url));
+})->with([
+    'https://instagram.com/__disco__dude',
+    'https://instagram.com/disco.dude',
+    'https://www.instagr.am/__disco__dude',
+]);
+
+it('invalidates an invalid url', function ($url) {
+    expect($this->subject->passes('instagram_url', $url))->toBeFalse();
+})->with([
+    'http://www.twitter.com/arkecosystem',
+    'arkecosystem',
+    '/arkecosystem',
+    'ftp://www.instagram.com/something',
+    'www.instagram.com/arkecosystem',
+]);
+
+it('has an error message', function () {
+    expect($this->subject->message())->toBe(trans('ui::validation.custom.instagram_url'));
+});
