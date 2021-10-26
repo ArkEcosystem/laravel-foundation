5ccf77cea629c1d19d92ab89fcc24cfc8051076f
alfonsobries <alfonsobries@gmail.com>
2021-10-26 11:51:38 -0500

    Add svg parser

---
+++ src/CommonMark/Extensions/Parser/Inline/SVGParser.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ARKEcosystem\Foundation\CommonMark\Extensions\Parser\Inline;
+
+use League\CommonMark\Parser\Inline\InlineParserInterface;
+use League\CommonMark\Parser\Inline\InlineParserMatch;
+use League\CommonMark\Parser\InlineParserContext;
+use League\CommonMark\Extension\CommonMark\Node\Inline\HtmlInline;
+
+final class SVGParser implements InlineParserInterface
+{
+    private const SVG_REGEX = '<(?:svg|SVG)[^>]*>';
+
+    public function getMatchDefinition(): InlineParserMatch
+    {
+        return InlineParserMatch::regex(self::SVG_REGEX);
+    }
+
+    public function parse(InlineParserContext $inlineContext): bool
+    {
+        $inlineContext->getCursor()->advanceBy($inlineContext->getFullMatchLength());
+
+        $match = $inlineContext->getFullMatch();
+
+        $inlineContext->getContainer()->appendChild(new HtmlInline($match));
+
+        return true;
+    }
+}
