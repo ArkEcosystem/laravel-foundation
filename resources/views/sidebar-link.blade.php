63f2711efe208d59f1a963c1b0880e7294771ccc
Stef Rouschop <stef.rouschop@coderunway.com>
2021-10-29 10:59:47 +0200

    added transition-default to sidebar-link

--- resources/views/sidebar-link.blade.php
+++ resources/views/sidebar-link.blade.php
@@ -19,7 +19,7 @@
         @else
             href="{{ route($route, $params) }}"
         @endif
-        class="flex items-center block font-semibold pl-8 py-3 space-x-2 @if($isCurrent) text-theme-primary-600 bg-theme-primary-100 @else text-theme-secondary-900 hover:text-theme-primary-600 @endif rounded-r w-full group"
+        class="flex items-center block font-semibold pl-8 py-3 space-x-2 @if($isCurrent) text-theme-primary-600 bg-theme-primary-100 @else text-theme-secondary-900 hover:text-theme-primary-600 @endif rounded-r w-full group transition-default"
         dusk='navbar-item-{{ Str::slug($name) }}'
         @foreach($attributes as $attribute => $attributeValue)
             {{ $attribute }}="{{ $attributeValue }}"
