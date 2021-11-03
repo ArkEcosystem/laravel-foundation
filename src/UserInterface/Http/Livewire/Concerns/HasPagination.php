1da7f89fde6e7ac8eb09606346634ebcf6f00d96
Chris Hipgrave <chrishipgrave@googlemail.com>
2021-11-03 12:14:39 +0000

    check requested page is numeric and if not default to 1 - this causes laravel to remove the query parameter from the url

--- src/UserInterface/Http/Livewire/Concerns/HasPagination.php
+++ src/UserInterface/Http/Livewire/Concerns/HasPagination.php
@@ -10,6 +10,17 @@ trait HasPagination
 {
     use WithPagination;
 
+    public function resolvePage()
+    {
+        if(request()->exists('page') && ! is_numeric(request()->query('page'))) {
+            $this->page = 1;
+            return $this->page;
+        }
+        // The "page" query string item should only be available
+        // from within the original component mount run.
+        return (int) request()->query('page', $this->page);
+    }
+
     public function gotoPage(int $page): void
     {
         $this->emit('pageChanged');
