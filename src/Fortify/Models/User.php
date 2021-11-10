26c56948a9fa131b02b83d8665fdadba3252d54a
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-11-10 19:30:02 +0000

    method to check user can moderate

--- src/Fortify/Models/User.php
+++ src/Fortify/Models/User.php
@@ -4,9 +4,16 @@ declare(strict_types=1);
 
 namespace ARKEcosystem\Foundation\Fortify\Models;
 
+use ARKEcosystem\Foundation\Fortify\Support\Enums\UserRole;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 class User extends UserWithoutVerification implements MustVerifyEmail
 {
-    //
+    public function canModerate(): bool
+    {
+        return $this->hasRole([
+            UserRole::SUPER_ADMIN,
+            UserRole::ADMIN,
+        ]);
+    }
 }
