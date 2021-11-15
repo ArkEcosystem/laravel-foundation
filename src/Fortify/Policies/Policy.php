a11483157ab2c69a22b716108dddfb4fac1dcd8f
alexbarnsley <alexbarnsley@users.noreply.github.com>
2021-11-15 10:26:56 +0000

    style: resolve style guide violations

--- src/Fortify/Policies/Policy.php
+++ src/Fortify/Policies/Policy.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace ARKEcosystem\Foundation\Fortify\Policies;
 
-use Illuminate\Database\Eloquent\Model;
-
 abstract class Policy
 {
     protected string $resourceName;
