05c7c8d4092a524f07f893aafb2fd3f7aa789f36
Maurizio <maurizio.lepora@gmail.com>
2021-11-17 10:42:56 +0100

    refactor: add "alert()" helper to flash a message on a session

---
+++ src/UserInterface/Support/Enums/FlashType.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ARKEcosystem\Foundation\UserInterface\Support\Enums;
+
+final class FlashType
+{
+    public const INFO    = 'alert-info';
+
+    public const SUCCESS = 'alert-success';
+
+    public const WARNING = 'alert-warning';
+
+    public const DANGER  = 'alert-danger';
+
+    public const ERROR   = 'alert-error';
+
+    public const HINT    = 'alert-hint';
+}
