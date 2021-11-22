aa68a0ec705a142be1516245655fdbc8568330ed
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-11-22 12:25:13 +0000

    copy across changes to header

--- resources/views/pages/includes/header.blade.php
+++ resources/views/pages/includes/header.blade.php
@@ -1,10 +1,11 @@
 @props([
-    'title'       => null,
-    'description' => null,
+    'title'        => null,
+    'description'  => null,
+    'contentClass' => 'flex flex-col items-center text-center lg:space-x-16 contact-header',
 ])
 
-<div class="bg-theme-secondary-100">
-    <div class="flex flex-col items-center text-center lg:space-x-16 contact-header">
+<div {{ $attributes->class('bg-theme-secondary-100') }}>
+    <div @class([$contentClass])>
         <h1 class="mb-0">{{ $title }}</h1>
 
         @if (strlen($description) > 0)
