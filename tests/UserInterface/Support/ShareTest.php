8d16a27563a615dce7a6d9107cbc28ae6a5ca98d
Maurizio <maurizio.lepora@gmail.com>
2021-11-26 10:58:06 +0100

    fix test

--- tests/UserInterface/Support/ShareTest.php
+++ tests/UserInterface/Support/ShareTest.php
@@ -3,10 +3,10 @@
 declare(strict_types=1);
 
 use ARKEcosystem\Foundation\UserInterface\Support\Share;
-use function Spatie\Snapshots\assertMatchesTextSnapshot;
+use function Spatie\Snapshots\assertMatchesSnapshot;
 
 it('shares url', function (string $service) {
-    assertMatchesTextSnapshot(
+    assertMatchesSnapshot(
         call_user_func_safe([Share::page('https://blog.my-website.com', 'Awesome Article'), $service])
     );
 })->with(['facebook', 'reddit', 'twitter']);
