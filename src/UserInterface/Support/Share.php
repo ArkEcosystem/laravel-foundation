58841c41bb61c8dbdd2304c7ee62333c87f272d8
Maurizio <maurizio.lepora@gmail.com>
2021-11-26 10:08:42 +0100

    feat: social share link

---
+++ src/UserInterface/Support/Share.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ARKEcosystem\Foundation\UserInterface\Support;
+
+final class Share
+{
+    public function __construct(
+        private ?string $url = null,
+        private ?string $title = null
+    )
+    {}
+
+    public static function page(string $url, ?string $title = null): self
+    {
+        return new static($url, $title);
+    }
+
+    public function facebook(): string
+    {
+        $base = config('share.services.facebook.uri');
+
+        return $this->buildLink($base, [
+            'u' => $this->url,
+        ]);
+    }
+
+    public function twitter(): string
+    {
+        if (is_null($this->title)) {
+            $this->title = config('laravel-share.services.twitter.text');
+        }
+
+        $base = config('share.services.twitter.uri');
+
+        return $this->buildLink($base, [
+            'text' => urlencode($this->title),
+            'url'  => $this->url,
+        ]);
+    }
+
+    public function reddit(): string
+    {
+        if (is_null($this->title)) {
+            $this->title = config('laravel-share.services.reddit.text');
+        }
+
+        $base = config('laravel-share.services.reddit.uri');
+
+        return $this->buildLink($base, [
+            'title' => urlencode($this->title),
+            'url'   => $this->url,
+        ]);
+    }
+
+    private function buildLink(string $url, array $option): string
+    {
+        return $url .= '?'.collect($option)
+                ->transform(fn ($value, $key) => $key.'='.$value)
+                ->join('&');
+    }
+}
