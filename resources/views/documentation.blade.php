be5511cb2505d5a778b8f4c40feb86253fb2f12a
Maurizio <maurizio.lepora@gmail.com>
2021-12-02 17:49:51 +0100

    use rem

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -1,5 +1,5 @@
 <x-ark-container>
-    <div {{ $attributes->class(['container mx-auto max-w-82ch simple-markdown']) }}>
+    <div {{ $attributes->class(['container mx-auto max-w-doc simple-markdown']) }}>
         {{ $slot }}
     </div>
 </x-ark-container>
