c760abc61d99231466d7254984849c86fc6fccca
alfonsobries <alfonsobries@users.noreply.github.com>
2021-12-08 23:31:44 +0000

    style: resolve style guide violations

--- resources/lang/en/markdown.php
+++ resources/lang/en/markdown.php
@@ -5,84 +5,84 @@ declare(strict_types=1);
 return [
     'navbar' => [
         'tooltips' => [
-            'more' => 'More',
-            'undo' => 'Undo',
-            'redo' => 'Redo',
-            'bold' => 'Bold',
-            'underline' => 'Underline',
-            'italic' => 'Italic',
-            'strike' => 'Strike',
-            'blockquote' => 'Blockquote',
-            'heading' => 'Heading :level',
-            'ordered_list' => 'Ordered List',
-            'unordered_list' => 'Unordered List',
-            'table' => 'Insert Table',
-            'image' => 'Insert Image',
-            'image' => 'Insert Image',
-            'link' => 'Insert Link',
-            'inline_code' => 'Inline Code',
-            'code_block' => 'Insert Code Block',
-            'embed_link' => 'Embed Link',
-            'embed_tweet' => 'Embed Tweet',
-            'simplecast' => 'Embed Simplecast',
-            'youtube' => 'Embed YouTube',
+            'more'            => 'More',
+            'undo'            => 'Undo',
+            'redo'            => 'Redo',
+            'bold'            => 'Bold',
+            'underline'       => 'Underline',
+            'italic'          => 'Italic',
+            'strike'          => 'Strike',
+            'blockquote'      => 'Blockquote',
+            'heading'         => 'Heading :level',
+            'ordered_list'    => 'Ordered List',
+            'unordered_list'  => 'Unordered List',
+            'table'           => 'Insert Table',
+            'image'           => 'Insert Image',
+            'image'           => 'Insert Image',
+            'link'            => 'Insert Link',
+            'inline_code'     => 'Inline Code',
+            'code_block'      => 'Insert Code Block',
+            'embed_link'      => 'Embed Link',
+            'embed_tweet'     => 'Embed Tweet',
+            'simplecast'      => 'Embed Simplecast',
+            'youtube'         => 'Embed YouTube',
             'link_collection' => 'Embed Link Collection',
-            'page_reference' => 'Embed Page Reference',
-            'alert' => 'Embed Alert',
+            'page_reference'  => 'Embed Page Reference',
+            'alert'           => 'Embed Alert',
         ],
     ],
     'modals' => [
         'embedLink' => [
             'title' => 'Add Embed Link',
-            'form' => [
-                'url' => 'URL',
+            'form'  => [
+                'url'     => 'URL',
                 'caption' => 'Caption',
-            ]
+            ],
         ],
         'embedTweet' => [
             'title' => 'Embed Tweet',
-            'form' => [
-                'url' => 'Tweet URL',
+            'form'  => [
+                'url'             => 'Tweet URL',
                 'url_placeholder' => 'https://twitter.com/arkecosystem/status/20',
-            ]
+            ],
         ],
         'embedPodcast' => [
             'title' => 'Embed Simplecast Podcast',
-            'form' => [
+            'form'  => [
                 'url' => 'Simplecast URL or ID',
-            ]
+            ],
         ],
         'embedYoutube' => [
             'title' => 'Embed YouTube video',
-            'form' => [
+            'form'  => [
                 'url' => 'Youtube URL or Video ID',
-            ]
+            ],
         ],
         'linkCollection' => [
             'title' => 'Link Collection',
-            'form' => [
+            'form'  => [
                 'name' => 'Name',
                 'path' => 'Path',
-            ]
+            ],
         ],
         'pageReference' => [
             'title' => 'Add Page Reference',
-            'form' => [
+            'form'  => [
                 'url' => 'Reference path or URL',
-            ]
+            ],
         ],
         'alert' => [
             'title' => 'Add Alert',
-            'form' => [
-                'type' => 'Alert type',
+            'form'  => [
+                'type'  => 'Alert type',
                 'types' => [
-                    'info' => 'Info',
+                    'info'    => 'Info',
                     'success' => 'Success',
                     'warning' => 'Warning',
-                    'danger' => 'Danger',
+                    'danger'  => 'Danger',
                 ],
                 'text' => 'Alert text',
-            ]
-        ]
-    ]
+            ],
+        ],
+    ],
 ];
