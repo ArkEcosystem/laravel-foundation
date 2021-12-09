0d0311423d6c6f37ada2af5610edb6b53308e088
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-12-09 20:34:55 +0000

    no need for inline flex :hide:

--- resources/views/chevron-open-close.blade.php
+++ resources/views/chevron-open-close.blade.php
@@ -3,11 +3,9 @@
     'size' => 'xs',
 ])
 
-<div class="inline-flex">
-    <span
-        {{ $attributes->class('transition duration-150 ease-in-out') }}
-        :class="{ 'rotate-180': {{ $isOpen }} }"
-    >
-        <x-ark-icon name="chevron-down" :size="$size" />
-    </span>
-</div>
+<span
+    {{ $attributes->class('transition duration-150 ease-in-out') }}
+    :class="{ 'rotate-180': {{ $isOpen }} }"
+>
+    <x-ark-icon name="chevron-down" :size="$size" />
+</span>
