b49d743f29560ccc2d4f9c0b8fa71bbc311f9a67
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-12-09 20:33:18 +0000

    wrap with inline flex class since the rotation doesn't work inside a normal div

--- resources/views/chevron-open-close.blade.php
+++ resources/views/chevron-open-close.blade.php
@@ -3,9 +3,11 @@
     'size' => 'xs',
 ])
 
-<span 
-    {{ $attributes->class('transition duration-150 ease-in-out transform') }}
-    :class="{ 'rotate-180': {{ $isOpen }} }"
->
-    <x-ark-icon name="chevron-down" :size="$size" />
-</span>
+<div class="inline-flex">
+    <span
+        {{ $attributes->class('transition duration-150 ease-in-out transform') }}
+        :class="{ 'rotate-180': {{ $isOpen }} }"
+    >
+        <x-ark-icon name="chevron-down" :size="$size" />
+    </span>
+</div>
