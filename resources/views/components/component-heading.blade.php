56aab7f4c7ac62bf23a32d8de85be69b5b5bfd82
alfonsobries <alfonsobries@users.noreply.github.com>
2021-12-10 23:45:26 +0000

    style: resolve style guide violations

--- resources/views/components/component-heading.blade.php
+++ resources/views/components/component-heading.blade.php
@@ -1,4 +1,4 @@
-<div class="py-8 w-full bg-theme-secondary-100 dark:bg-black">
+<div class="py-8 w-full dark:bg-black bg-theme-secondary-100">
     <div class="container mx-auto">
         <h1 class="mx-4 text-center md:mx-8 xl:mx-16 dark:text-theme-secondary-200">{{ $title }}</h1>
         <p class="mx-8 mt-4 text-lg font-semibold leading-8 text-center md:mx-8 xl:mx-16 text-theme-secondary-700 dark:text-theme-secondary-500">{{ $description }}</p>
