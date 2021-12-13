9f275353437b9ee20d37d195424fe48e77282b15
Maurizio <leMaur@users.noreply.github.com>
2021-12-13 18:51:07 +0100

    refactor: adjust vertical form spacing (#180)

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -16,37 +16,33 @@
 @section('content')
     <x-data-bag key="fortify-content" resolver="name" view="ark-fortify::components.component-heading"/>
 
-    <div class="py-8 mx-auto max-w-xl">
-        <div class="px-8">
-            <x-ark-flash/>
-        </div>
+    <div class="py-8 mx-auto space-y-8 max-w-xl">
+        <x-ark-flash />
 
-        <div class="mt-5 lg:mt-8">
-            <x:ark-fortify::form-wrapper :action="route('password.email')">
-                <div class="mb-8">
-                    <div class="flex flex-1">
-                        <x-ark-input
-                            type="email"
-                            name="email"
-                            label="Email"
-                            autocomplete="email"
-                            class="w-full"
-                            :autofocus="true"
-                            :required="true"
-                        />
-                    </div>
+        <x:ark-fortify::form-wrapper class="sm:max-w-xl" :action="route('password.email')">
+            <div class="mb-8">
+                <div class="flex flex-1">
+                    <x-ark-input
+                        type="email"
+                        name="email"
+                        label="Email"
+                        autocomplete="email"
+                        class="w-full"
+                        :autofocus="true"
+                        :required="true"
+                    />
+                </div>
+            </div>
+
+            <div class="flex flex-col-reverse justify-between items-center space-y-4 md:flex-row md:space-y-0">
+                <div class="flex-1 mt-8 md:mt-0">
+                    <a href="{{ route('login') }}" class="link">@lang('ui::actions.cancel')</a>
                 </div>
 
-                <div class="flex flex-col-reverse justify-between items-center space-y-4 md:flex-row md:space-y-0">
-                    <div class="flex-1 mt-8 md:mt-0">
-                        <a href="{{ route('login') }}" class="link">@lang('ui::actions.cancel')</a>
-                    </div>
-
-                    <button type="submit" class="w-full md:w-auto button-secondary">
-                        @lang('ui::auth.forgot-password.reset_link')
-                    </button>
-                </div>
-            </x:ark-fortify::form-wrapper>
-        </div>
+                <button type="submit" class="w-full md:w-auto button-secondary">
+                    @lang('ui::auth.forgot-password.reset_link')
+                </button>
+            </div>
+        </x:ark-fortify::form-wrapper>
     </div>
 @endsection
