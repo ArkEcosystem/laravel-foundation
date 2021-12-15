44337853ff5f0e92545f75212edbd6b967bd07e9
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-12-15 00:01:53 +0000

    update shapes

--- resources/views/shapes/line.blade.php
+++ resources/views/shapes/line.blade.php
@@ -1,3 +1,13 @@
+@props([
+    'colorClass'   => 'bg-theme-secondary-200 dark:bg-theme-secondary-900',
+    'sizeClass'    => 'w-full h-3',
+    'roundedClass' => 'rounded-xl',
+])
+
 <div>
-    <div class="{{ $colorClass ?? 'bg-theme-secondary-200' }} {{ $sizeClass ?? 'w-full h-3' }} {{ $roundedClass ?? 'rounded-xl' }}"></div>
+    <div @class([
+        $colorClass,
+        $sizeClass,
+        $roundedClass,
+    ])></div>
 </div>
