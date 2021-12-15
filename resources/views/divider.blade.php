f2e2de1c20987c520ee7d2917280676d830361ca
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-12-15 10:17:02 +0000

    refactor: dark mode cancel button, divider, placeholders, etc (#191)

--- resources/views/divider.blade.php
+++ resources/views/divider.blade.php
@@ -1,3 +1,6 @@
 @props(['height' => 'h-px'])
 
-<hr class="bg-theme-secondary-300 text-theme-secondary-300 w-full border-none {{ $height }}" />
+<hr {{ $attributes->class([
+    'bg-theme-secondary-300 text-theme-secondary-300 w-full border-none dark:bg-theme-secondary-800',
+    $height,
+]) }} />
