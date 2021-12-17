47c0eb38371cb84bc2af30475606bd87e24b583d
Maurizio <leMaur@users.noreply.github.com>
2021-12-17 13:18:50 +0100

    feat: script to manage "dark theme" mode (#198)

---
+++ resources/views/scripts/dark-theme-script.blade.php
@@ -0,0 +1,23 @@
+@if(config('ui.dark-mode.enabled') === true)
+    <script>
+        document.addEventListener('setThemeMode', (e) => {
+            localStorage.theme = e.detail.theme; toggleTheme();
+        });
+
+        document.addEventListener('setOSThemeMode', (e) => {
+            localStorage.removeItem('theme'); toggleTheme();
+        });
+
+        document.addEventListener('toggleThemeMode', (e) => toggleTheme());
+
+        const toggleTheme = () => {
+            if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                document.documentElement.classList.add('dark')
+            } else {
+                document.documentElement.classList.remove('dark')
+            }
+        }
+
+        toggleTheme();
+    </script>
+@endif
