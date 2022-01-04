a51988b3c7e7a9ad83e222b24ff048aef2601328
alfonsobries <alfonsobries@users.noreply.github.com>
2022-01-04 16:48:29 +0000

    style: resolve style guide violations

--- resources/views/js-modal.blade.php
+++ resources/views/js-modal.blade.php
@@ -26,7 +26,7 @@
     tabindex="0"
     @endif
     x-show="shown"
-    class="fixed inset-0 z-50 flex overflow-y-auto md:py-10 md:px-8"
+    class="flex overflow-y-auto fixed inset-0 z-50 md:py-10 md:px-8"
 >
 
     <div class="fixed inset-0 opacity-75 dark:opacity-50 bg-theme-secondary-900 dark:bg-theme-secondary-800"></div>
