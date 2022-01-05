c7428b82d273695d5c016843fe602992937233f1
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2022-01-05 14:44:22 +0000

    component to handle frontend settings

---
+++ src/UserInterface/Components/FrontendSettings.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ARKEcosystem\Foundation\UserInterface\Components;
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Cookie;
+use Livewire\Component;
+
+class FrontendSettings extends Component
+{
+    public array $state = [];
+
+    protected array $defaultSettings = [
+        'darkTheme' => false,
+    ];
+
+    protected $listeners = [
+        'settings.toggle-dark-mode' => 'toggleDarkMode',
+    ];
+
+    private function cookieSettings(): array
+    {
+        $sessionSettings = [];
+        if (Cookie::has('settings')) {
+            $sessionSettings = json_decode(strval(Cookie::get('settings')), true);
+        }
+
+        return $sessionSettings + $this->defaultSettings;
+    }
+
+    public function mount(): void
+    {
+        $this->state = $this->cookieSettings();
+    }
+
+    public function render()
+    {
+        return '<div></div>';
+    }
+
+    public function toggleDarkMode(): void
+    {
+        $this->state['darkTheme'] = ! $this->state['darkTheme'];
+        $this->saveSettings();
+    }
+
+    private function saveSettings(): void
+    {
+        $originalTheme = Arr::get($this->cookieSettings(), 'darkTheme');
+        $newTheme      = Arr::get($this->state, 'darkTheme');
+
+        Cookie::queue('settings', json_encode($this->state), 60 * 24 * 365 * 5);
+
+        if ($originalTheme !== $newTheme) {
+            $this->dispatchBrowserEvent('setThemeMode', [
+                'theme' => $newTheme === true ? 'dark' : 'light',
+            ]);
+        }
+    }
+}
