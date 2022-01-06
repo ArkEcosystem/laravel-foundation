9d65fcc2aa1886828d1cd53c6406e532ea896d60
Stef Rouschop <stef@rouschop.com>
2022-01-06 11:53:33 +0100

    changed checkbox bg

--- resources/views/components/manage-notifications.blade.php
+++ resources/views/components/manage-notifications.blade.php
@@ -7,7 +7,7 @@
                 <div class="relative">
                     <button type="button" class="flex justify-center items-center w-10 h-10 rounded border border-solid cursor-pointer focus:outline-none text-theme-secondary-400 border-theme-secondary-200 dark:border-theme-secondary-800 hover:text-theme-primary-500" wire:click="{{ $this->hasAllSelected ? 'deselectAllNotifications' : 'selectAllNotifications' }}">
                         @if($this->hasAllSelected)
-                            <div class="flex justify-center items-center w-5 h-5 text-white rounded bg-theme-success-500 dark:bg-theme-success-600">
+                            <div class="flex justify-center items-center w-5 h-5 text-white rounded bg-theme-success-600">
                                 <x-ark-icon name="checkmark" size="2xs" />
                             </div>
                         @else
@@ -120,7 +120,7 @@
                                 <button
                                     type="button"
                                     wire:click.stop="$emit('setNotification', '{{ $notification->id }}')"
-                                    class="flex justify-center items-center w-5 h-5 text-white rounded cursor-pointer box-border bg-theme-success-500 dark:bg-theme-success-600"
+                                    class="flex justify-center items-center w-5 h-5 text-white rounded cursor-pointer box-border bg-theme-success-600"
                                 >
                                     <x-ark-icon name="checkmark" size="2xs" />
                                 </button>
