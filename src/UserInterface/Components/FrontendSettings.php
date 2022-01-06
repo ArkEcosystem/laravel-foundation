bd3e94c2fb901ac0a0ddb41ddacbbbd7201d7d19
alexbarnsley <alexbarnsley@users.noreply.github.com>
2022-01-06 23:37:24 +0000

    style: resolve style guide violations

--- src/UserInterface/Components/FrontendSettings.php
+++ src/UserInterface/Components/FrontendSettings.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace ARKEcosystem\Foundation\UserInterface\Components;
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Cookie;
 use Livewire\Component;
 
