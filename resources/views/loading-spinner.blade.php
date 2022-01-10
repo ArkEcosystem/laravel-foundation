d8185e4785216081099290ef570c4fbe3e189d44
Maurizio <maurizio.lepora@gmail.com>
2022-01-10 11:11:02 +0100

    update loading-spinner.blade.php

--- resources/views/loading-spinner.blade.php
+++ resources/views/loading-spinner.blade.php
@@ -5,5 +5,5 @@
 ])
 
 <div class="cursor-pointer flex items-center justify-center absolute top-0 opacity-90 transition-default bg-theme-secondary-900 {{ $dimensions }} {{ $class }}">
-    <x-ark-icon name="oval" class=" {{ $spinnerDimensions }} text-white" />
+    <x-ark-icon name="circle.spinner" class=" {{ $spinnerDimensions }} text-white" />
 </div>
