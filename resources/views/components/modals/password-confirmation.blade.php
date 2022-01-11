92f0588171cbbb3fd69ac331bb196c996bb2677c
faustbrian <faustbrian@users.noreply.github.com>
2022-01-11 12:55:44 +0000

    style: resolve style guide violations

--- resources/views/components/modals/password-confirmation.blade.php
+++ resources/views/components/modals/password-confirmation.blade.php
@@ -26,7 +26,7 @@
                         alt=""
                     />
                 @elseif ($icon)
-                    <x-ark-icon :name="$icon" class="h-28 w-full light-dark-icon" />
+                    <x-ark-icon :name="$icon" class="w-full h-28 light-dark-icon" />
                 @endif
             </div>
 
