422eba49f24bb58e223e2241a0555294e6462e9f
Maurizio <maurizio.lepora@gmail.com>
2022-01-27 16:25:42 +0100

    update DateFormat.php

--- src/UserInterface/Support/DateFormat.php
+++ src/UserInterface/Support/DateFormat.php
@@ -4,21 +4,21 @@ declare(strict_types=1);
 
 namespace ARKEcosystem\Foundation\UserInterface\Support;
 
-class DateFormat
+enum DateFormat: string
 {
-    public const DATE = 'd M Y';
+    case DATE = 'd M Y';
 
-    public const TIME = 'd M Y H:i:s';
+    case TIME = 'd M Y H:i:s';
 
-    public const TIME_SHORT = 'd M Y H:i';
+    case TIME_SHORT = 'd M Y H:i';
 
-    public const TIME_PARENTHESES = 'd M Y (H:i:s)';
+    case TIME_PARENTHESES = 'd M Y (H:i:s)';
 
-    public const DATE_JS = 'D MMM YYYY';
+    case DATE_JS = 'D MMM YYYY';
 
-    public const TIME_JS = 'D MMM YYYY HH:mm:ss';
+    case TIME_JS = 'D MMM YYYY HH:mm:ss';
 
-    public const TIME_SHORT_JS = 'D MMM YYYY HH:mm';
+    case TIME_SHORT_JS = 'D MMM YYYY HH:mm';
 
-    public const TIME_PARENTHESES_JS = 'D MMM YYYY (HH:mm:ss)';
+    case TIME_PARENTHESES_JS = 'D MMM YYYY (HH:mm:ss)';
 }
