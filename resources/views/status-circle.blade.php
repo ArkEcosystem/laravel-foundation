48ab46a76e29767aa070e8de760efc3b3716fce3
Maurizio <leMaur@users.noreply.github.com>
2022-01-31 15:02:33 +0100

    refactor: adjust status-circle component sizes (#262)

--- resources/views/status-circle.blade.php
+++ resources/views/status-circle.blade.php
@@ -1,25 +1,25 @@
 @if ($type === 'success')
-    <div class="flex flex-shrink-0 justify-center items-center w-6 h-6 rounded-full bg-theme-success-200">
-        <x-ark-icon name="check-mark-bold" size="xs" class="text-theme-success-500" />
+    <div class="flex flex-shrink-0 justify-center items-center w-5 h-5 rounded-full bg-theme-success-200">
+        <x-ark-icon name="check-mark-small" size="2xs" class="text-theme-success-500" />
     </div>
 @elseif ($type === 'failed' || $type === 'error')
-    <div class="flex flex-shrink-0 justify-center items-center w-6 h-6 rounded-full bg-theme-danger-100">
+    <div class="flex flex-shrink-0 justify-center items-center w-5 h-5 rounded-full bg-theme-danger-100">
         <x-ark-icon name="cross" size="2xs" class="text-theme-danger-500" />
     </div>
 @elseif ($type === 'running')
-    <div class="flex flex-shrink-0 justify-center items-center w-6 h-6 rounded-full bg-theme-warning-100">
+    <div class="flex flex-shrink-0 justify-center items-center w-5 h-5 rounded-full bg-theme-warning-100">
         <x-ark-icon name="arrows.arrows-rotate" size="xs" class="text-theme-warning-900 animation-spin" />
     </div>
 @elseif ($type === 'updated')
-    <div class="flex flex-shrink-0 justify-center items-center w-6 h-6 rounded-full bg-theme-warning-100">
+    <div class="flex flex-shrink-0 justify-center items-center w-5 h-5 rounded-full bg-theme-warning-100">
         <x-ark-icon name="arrows.arrows-rotate" size="xs" class="text-theme-warning-900" />
     </div>
 @elseif ($type === 'active')
-    <div class="flex flex-shrink-0 justify-center items-center w-6 h-6 rounded-full border-2 box-border border-theme-primary-500"></div>
+    <div class="flex flex-shrink-0 justify-center items-center rounded-full border-2 w-4.5 h-4.5 box-border border-theme-primary-500"></div>
 @elseif ($type === 'locked')
-    <div class="flex flex-shrink-0 justify-center items-center w-6 h-6 rounded-full bg-theme-secondary-300">
+    <div class="flex flex-shrink-0 justify-center items-center w-5 h-5 rounded-full bg-theme-secondary-300">
         <x-ark-icon name="lock" size="xs" class="text-theme-secondary-700" />
     </div>
 @else
-    <div class="flex flex-shrink-0 justify-center items-center w-6 h-6 rounded-full bg-theme-secondary-300"></div>
+    <div class="flex flex-shrink-0 justify-center items-center w-5 h-5 rounded-full bg-theme-secondary-300"></div>
 @endif
