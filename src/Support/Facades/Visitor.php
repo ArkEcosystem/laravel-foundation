c7832abe7fe4475887f7d23502f3fafdc200e0ed
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2022-02-09 19:07:03 +0000

    european visitor class

---
+++ src/Support/Facades/Visitor.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ARKEcosystem\Foundation\Support\Facades;
+
+use ARKEcosystem\Foundation\Support\Services\Visitor as Service;
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static self isEuropean()
+ * 
+ * @see \ARKEcosystem\Foundation\Support\Services\Visitor
+ */
+class Visitor extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return Service::class;
+    }
+}
