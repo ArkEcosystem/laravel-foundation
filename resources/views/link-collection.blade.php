e689a7c9daaabba186e171b61111d4930b187c99
alexbarnsley <alexbarnsley@users.noreply.github.com>
2022-03-09 13:36:57 +0000

    style: resolve style guide violations

--- resources/views/link-collection.blade.php
+++ resources/views/link-collection.blade.php
@@ -9,16 +9,16 @@
         <div class="py-1">
             <a
                 href="{{ $link[$urlProperty] }}"
-                class="flex group no-underline pl-5 h-full w-full rounded transition-default border-2 border-theme-primary-100 hover:bg-theme-primary-700 hover:border-theme-primary-700"
+                class="flex pl-5 w-full h-full no-underline rounded border-2 group transition-default border-theme-primary-100 hover:bg-theme-primary-700 hover:border-theme-primary-700"
                 @if ($isExternal)
                     target="_blank"
                     rel="noopener nofollow noreferrer"
                 @endif
             >
-                <div class="flex flex-1 justify-between items-center text-theme-primary-600 group-hover:text-white">
+                <div class="flex flex-1 justify-between items-center group-hover:text-white text-theme-primary-600">
                     <span class="py-2">{{ $link['name'] }}</span>
 
-                    <div class="flex items-center justify-center bg-theme-primary-100 group-hover:bg-transparent w-11 h-full -my-px">
+                    <div class="flex justify-center items-center -my-px w-11 h-full group-hover:bg-transparent bg-theme-primary-100">
                         <x-ark-icon name="arrows.chevron-right" size="sm" />
                     </div>
                 </div>
