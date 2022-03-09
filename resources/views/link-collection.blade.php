a95d22014feb3467161260f7c68c54663dc63cc1
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2022-03-09 13:36:09 +0000

    refactor: update link collection style

--- resources/views/link-collection.blade.php
+++ resources/views/link-collection.blade.php
@@ -1,9 +1,27 @@
-<div class="grid grid-cols-1 grid-flow-row gap-x-3 sm:grid-cols-2 lg:grid-cols-3 link-collection">
+@props([
+    'links',
+    'urlProperty' => 'path',
+    'isExternal'  => false,
+])
+
+<div class="grid grid-cols-1 grid-flow-row gap-x-2 sm:grid-cols-2 lg:grid-cols-3 link-collection">
     @foreach ($links as $link)
         <div class="py-1">
-            <a href="{{ $link[ $urlProperty ?? 'path'] }}" class="flex justify-between items-center py-3 px-2 w-full rounded transition-default text-theme-primary-600 hover:bg-theme-primary-100 hover:text-theme-primary-700" @if ($isExternal ?? false) target="_blank" rel="noopener nofollow noreferrer" @endif>
-                <span>{{ $link['name'] }}</span>
-                <x-ark-icon name="arrows.arrow-right" size="md" />
+            <a
+                href="{{ $link[$urlProperty] }}"
+                class="flex group no-underline pl-5 h-full w-full rounded transition-default border-2 border-theme-primary-100 hover:bg-theme-primary-700 hover:border-theme-primary-700"
+                @if ($isExternal)
+                    target="_blank"
+                    rel="noopener nofollow noreferrer"
+                @endif
+            >
+                <div class="flex flex-1 justify-between items-center text-theme-primary-600 group-hover:text-white">
+                    <span class="py-2">{{ $link['name'] }}</span>
+
+                    <div class="flex items-center justify-center bg-theme-primary-100 group-hover:bg-transparent w-11 h-full -my-px">
+                        <x-ark-icon name="arrows.chevron-right" size="sm" />
+                    </div>
+                </div>
             </a>
         </div>
     @endforeach
