b3c452e0d0f9f23eb467635a44dc24e8bfdc604d
Josip Crnković <jcrnkovic95@gmail.com>
2022-04-08 17:17:37 +0200

    Update navbar.js

--- resources/assets/js/navbar.js
+++ resources/assets/js/navbar.js
@@ -13,6 +13,7 @@ const onNavbarOpened = (navbar) => {
 const Navbar = {
     dropdown(xData = {}) {
         return {
+            inverted: false,
             open: false,
             openDropdown: null,
             selectedChild: null,
@@ -20,13 +21,21 @@ const Navbar = {
             nav: null,
             dark: false,
             lockBodyBreakpoint: 640,
+
+            // Colors...
+            white: [255, 255, 255],
+            black: [0, 0, 0],
+            initialBackgroundColor: null,
+
             onScroll() {
                 const progress = this.getScrollProgress();
+
                 if (progress !== this.scrollProgress) {
                     this.scrollProgress = progress;
-                    this.updateShadow(progress);
+                    this.updateStyles(progress);
                 }
             },
+
             getScrollProgress() {
                 const navbarHeight = 82;
                 return Math.min(
@@ -34,32 +43,32 @@ const Navbar = {
                     document.documentElement.scrollTop / navbarHeight
                 );
             },
-            updateShadow(progress) {
-                const maxTransparency = this.dark ? 0.6 : 0.22;
-                const shadowTransparency =
-                    Math.round(maxTransparency * progress * 100) / 100;
-                const borderTransparency =
-                    Math.round((1 - progress) * 100) / 100;
-                const borderColorRgb = this.dark
-                    ? [60, 66, 73]
-                    : [219, 222, 229];
-                const boxShadowRgb = this.dark ? [18, 18, 19] : [192, 200, 207];
-                this.nav.style.boxShadow = `0px 2px 10px 0px rgba(${boxShadowRgb.join(
-                    ", "
-                )}, ${shadowTransparency})`;
-                this.nav.style.borderColor = `rgba(${borderColorRgb.join(
-                    ", "
-                )}, ${borderTransparency})`;
+
+            updateStyles(progress) {
+                this.updateShadow(progress)
+
+                if (this.inverted) {
+                    this.invertColorScheme(progress)
+                }
             },
+
             init() {
                 const { nav, scrollable } = this.$refs;
                 this.nav = nav;
+
+                // Register initial colors...
+                if (this.inverted) {
+                    this.targetLogoColor = [...this.white];
+                    this.initialBackgroundColor = this.getColorValues(this.getElementStyle(nav, 'backgroundColor'));
+                }
+
                 window.onscroll = this.onScroll.bind(this);
                 this.scrollProgress = this.getScrollProgress();
-                this.updateShadow(this.scrollProgress);
+
+                this.updateStyles(this.scrollProgress);
 
                 this.$watch("dark", () =>
-                    this.updateShadow(this.getScrollProgress())
+                    this.updateStyles(this.getScrollProgress())
                 );
 
                 this.$watch("open", (open) => {
@@ -78,6 +87,61 @@ const Navbar = {
                     onNavbarOpened(scrollable || nav);
                 }
             },
+
+            updateShadow(progress) {
+                const maxTransparency = this.dark ? 0.6 : 0.22;
+                const shadowTransparency = Math.round(maxTransparency * progress * 100) / 100;
+                const borderTransparency = Math.round((1 - progress) * 100) / 100;
+                const borderColorRgb = this.dark ? [60, 66, 73] : [219, 222, 229];
+                const boxShadowRgb = this.dark ? [18, 18, 19] : [192, 200, 207];
+                this.nav.style.boxShadow = `0px 2px 10px 0px rgba(${boxShadowRgb.join(", ")}, ${shadowTransparency})`;
+                this.nav.style.borderColor = `rgba(${borderColorRgb.join(", ")}, ${borderTransparency})`;
+            },
+
+            getColorTransition(start, end, opacity) {
+                if (opacity === 0) {
+                    return [...start]
+                }
+
+                if (opacity === 1) {
+                    return [...end]
+                }
+
+                return [...end]
+                    .map((color, index) => (color > start[index] ? start[index] : color) + Math.abs((color - start[index]) * (color > start[index] ? opacity : (1-opacity))))
+                    .map(Math.ceil)
+                    .join(',');
+            },
+
+            getElementStyle: (element, property) => document.defaultView.getComputedStyle(element, null)[property],
+            computeColor(color) {
+                return this.hex2rgb(getComputedStyle(document.documentElement).getPropertyValue(color).replace('#', ''))
+            },
+            getColorValues: text => {
+                if (text.startsWith('rgb(')) {
+                    text = text.replace(/\)/, '')
+                    text = text.replace(/rgb\(/, '')
+                }
+
+                return text.split(', ').map(Number)
+            },
+            hex2rgb(hex) {
+                const bigint = parseInt(hex, 16);
+
+                return [
+                    (bigint >> 16) & 255,
+                    (bigint >> 8) & 255,
+                    bigint & 255,
+                ];
+            },
+
+            invertColorScheme(progress) {
+                console.log(this.$refs.logo)
+
+                this.nav.style.backgroundColor = `rgb(${this.getColorTransition(this.initialBackgroundColor, this.white, progress)})`
+                this.$refs.siteName.style.color = `rgb(${this.getColorTransition(this.white, this.computeColor('--theme-color-secondary-900'), progress)})`
+            },
+
             toggleDropdown(name) {
                 this.openDropdown = this.openDropdown === name ? null : name;
             },
