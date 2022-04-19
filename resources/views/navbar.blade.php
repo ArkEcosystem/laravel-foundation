994c2a3245049b49b6307005ef8ebefeb59372bb
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2022-04-19 11:05:03 +0100

    revert bottom navbar colour

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -52,9 +52,9 @@
         aria-label="{{ trans('ui::general.primary_navigation') }}"
         x-ref="nav"
         @class([
-        	'fixed top-0 z-30 w-full border-b dark:bg-theme-secondary-900 dark:border-theme-secondary-800 transition duration-400',
-           'border-theme-primary-700 bg-theme-primary-600 inverted:bg-white inverted:border-transparent inverted:shadow-header-smooth' => $inverted,
-        	'bg-white border-theme-secondary-300' => ! $inverted,
+            'fixed top-0 z-30 w-full border-b dark:bg-theme-secondary-900 dark:border-theme-secondary-800 transition duration-400',
+            'border-theme-primary-500 bg-theme-primary-600 inverted:bg-white inverted:border-transparent inverted:shadow-header-smooth' => $inverted,
+            'bg-white border-theme-secondary-300' => ! $inverted,
         ])
         dusk="navigation-bar"
     >
