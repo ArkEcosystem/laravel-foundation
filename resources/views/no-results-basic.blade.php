3a705f7010f973f143dffe0cad8fed02a965f340
alexbarnsley <alexbarnsley@users.noreply.github.com>
2022-04-19 23:42:36 +0000

    style: resolve style guide violations

--- resources/views/no-results-basic.blade.php
+++ resources/views/no-results-basic.blade.php
@@ -3,7 +3,7 @@
 ])
 
 <div {{ $attributes->class('flex flex-col justify-center justify-between items-center py-4 px-6 rounded-xl border sm:items-start md:flex-row md:items-center border-theme-secondary-300 dark:border-theme-secondary-800 dark:text-theme-secondary-500') }}>
-    <span class="text-center w-full">
+    <span class="w-full text-center">
         {{ $text }}
     </span>
 </div>
