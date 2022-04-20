86e135e9f9f8ada29f0592c0e831c86a9ea2c837
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2022-04-20 14:06:35 +0100

    tests

--- tests/Documentation/DocumentTest.php
+++ tests/Documentation/DocumentTest.php
@@ -36,61 +36,124 @@ it('should render', function () {
 });
 
 it('should not highlight intro in sidebar', function () {
-    app()->instance('request', createMockRequest('/docs/desktop-wallet/installation'));
+    app()->instance('request', createMockRequest('/docs/installation'));
 
     $document = Document::find('0d6eaf5f0b12c40882e0a648eecec8e5');
 
-    expect($document->body)->toContain('<x-ark-docs-sidebar-link first top-level path="/docs/desktop-wallet/intro" name="Introduction" />');
+    expect($document->body)->toContain('<x-ark-docs-sidebar-link first top-level path="/docs/intro" name="Introduction" />');
 
     expect(Blade::render($document->body))->not->toMatch(
         '/<div class=".+bg-theme-primary-600.+"><\/div>\s+'.
         '<div class="rounded-r w-full pl-4 lg:pl-5 text-theme-primary-600 bg-theme-primary-100">\s+'.
-        '<a\s+href="\/docs\/desktop-wallet\/intro" class="border-theme-secondary-300 py-4 flex items-center block font-semibold w-full lg:border-t"/'
+        '<a\s+href="\/docs\/intro" class="border-theme-secondary-300 py-4 flex items-center block font-semibold w-full lg:border-t"/'
     );
 
     expect(Blade::render($document->body))->toMatch(
         '/<div class="w-1 -mr-1 z-10"><\/div>\s+'.
         '<div class="rounded-r w-full pl-4 lg:pl-5 text-theme-secondary-900 hover:text-theme-primary-600">\s+'.
-        '<a\s+href="\/docs\/desktop-wallet\/intro"/'
+        '<a\s+href="\/docs\/intro"/'
     );
 });
 
 it('should highlight intro in sidebar when at root category url', function () {
-    app()->instance('request', createMockRequest('/docs/desktop-wallet'));
+    app()->instance('request', createMockRequest('/docs'));
 
     $document = Document::find('0d6eaf5f0b12c40882e0a648eecec8e5');
 
-    expect($document->body)->toContain('<x-ark-docs-sidebar-link first top-level path="/docs/desktop-wallet/intro" name="Introduction" />');
+    expect($document->body)->toContain('<x-ark-docs-sidebar-link first top-level path="/docs/intro" name="Introduction" />');
 
     expect(Blade::render($document->body))->not->toMatch(
         '/<div class="w-1 -mr-1 z-10"><\/div>\s+'.
         '<div class="rounded-r w-full pl-4 lg:pl-5 text-theme-secondary-900 hover:text-theme-primary-600">\s+'.
-        '<a\s+href="\/docs\/desktop-wallet\/intro"/'
+        '<a\s+href="\/docs\/intro"/'
     );
 
     expect(Blade::render($document->body))->toMatch(
         '/<div class=".+bg-theme-primary-600.+"><\/div>\s+'.
         '<div class="rounded-r w-full pl-4 lg:pl-5 text-theme-primary-600 bg-theme-primary-100">\s+'.
-        '<a\s+href="\/docs\/desktop-wallet\/intro"\s+class="border-theme-secondary-300 py-4 flex items-center block font-semibold w-full lg:border-t"/'
+        '<a\s+href="\/docs\/intro"\s+class="border-theme-secondary-300 py-4 flex items-center block font-semibold w-full lg:border-t"/'
     );
 });
 
 it('should highlight intro in sidebar when at category intro url', function () {
-    app()->instance('request', createMockRequest('/docs/desktop-wallet/intro'));
+    app()->instance('request', createMockRequest('/docs/intro'));
 
     $document = Document::find('0d6eaf5f0b12c40882e0a648eecec8e5');
 
-    expect($document->body)->toContain('<x-ark-docs-sidebar-link first top-level path="/docs/desktop-wallet/intro" name="Introduction" />');
+    expect($document->body)->toContain('<x-ark-docs-sidebar-link first top-level path="/docs/intro" name="Introduction" />');
 
     expect(Blade::render($document->body))->not->toMatch(
         '/<div class="w-1 -mr-1 z-10"><\/div>\s+'.
         '<div class="rounded-r w-full pl-4 lg:pl-5 text-theme-secondary-900 hover:text-theme-primary-600">\s+'.
-        '<a\s+href="\/docs\/desktop-wallet\/intro"/'
+        '<a\s+href="\/docs\/intro"/'
+    );
+
+    expect(Blade::render($document->body))->toMatch(
+        '/<div class="w-1 -mr-1 z-10 border-l border-theme-secondary-300"><\/div>\s+'.
+        '<a\s+href="\/docs\/user-guides\/transactions\/how-to-vote-unvote"/'
     );
 
     expect(Blade::render($document->body))->toMatch(
         '/<div class=".+bg-theme-primary-600.+"><\/div>\s+'.
         '<div class="rounded-r w-full pl-4 lg:pl-5 text-theme-primary-600 bg-theme-primary-100">\s+'.
-        '<a\s+href="\/docs\/desktop-wallet\/intro"\s+class="border-theme-secondary-300 py-4 flex items-center block font-semibold w-full lg:border-t"/'
+        '<a\s+href="\/docs\/intro"\s+class="border-theme-secondary-300 py-4 flex items-center block font-semibold w-full lg:border-t"/'
+    );
+});
+
+it('should highlight different page in sidebar', function () {
+    app()->instance('request', createMockRequest('/docs/user-guides/transactions/how-to-vote-unvote'));
+
+    $document = Document::find('0d6eaf5f0b12c40882e0a648eecec8e5');
+
+    expect($document->body)->toContain('<x-ark-docs-sidebar-link path="/docs/user-guides/transactions/how-to-vote-unvote" name="Vote or Unvote a Delegate" />');
+
+    expect(Blade::render($document->body))->not->toMatch(
+        '/<div class="w-1 -mr-1 z-10 border-l border-theme-secondary-300"><\/div>\s+'.
+        '<a\s+href="\/docs\/user-guides\/transactions\/how-to-vote-unvote"/'
+    );
+
+    expect(Blade::render($document->body))->toMatch(
+        '/<div class=".+bg-theme-primary-600.+"><\/div>\s+'.
+        '<a\s+href="\/docs\/user-guides\/transactions\/how-to-vote-unvote"/'
+    );
+});
+
+it('should expand getting started section in sidebar', function () {
+    app()->instance('request', createMockRequest('/docs/user-guides/getting-started/installation'));
+
+    $document = Document::find('0d6eaf5f0b12c40882e0a648eecec8e5');
+
+    expect(Blade::render($document->body))->not->toMatch(
+        '/<div\s+class="lg:ml-8"\s+x-data="{ open: true }"\s+:class="{ \'last:pb-4\': ! open }"\s+x-cloak\s+>\s+'.
+        '<button\s+type="button"\s+class="flex items-center justify-between w-full pl-5 lg:pl-0 pr-5 border-theme-secondary-300 group pt-4 mt-4 border-t"\s+'.
+        '@click.prevent="open = ! open"\s+>\s+'.
+        '<h2 class="mb-0 text-base font-semibold text-left accordion-heading text-theme-secondary-900 group-hover:text-theme-primary-600">\s+Transactions/'
+    );
+
+    expect(Blade::render($document->body))->toMatch(
+        '/<div\s+class="lg:ml-8"\s+x-data="{ open: true }"\s+:class="{ \'last:pb-4\': ! open }"\s+x-cloak\s+>\s+'.
+        '<button\s+type="button"\s+class="flex items-center justify-between w-full pl-5 lg:pl-0 pr-5 border-theme-secondary-300 group pt-4 mt-4 border-t"\s+'.
+        '@click.prevent="open = ! open"\s+>\s+'.
+        '<h2 class="mb-0 text-base font-semibold text-left accordion-heading text-theme-secondary-900 group-hover:text-theme-primary-600">\s+Getting Started/'
+    );
+});
+
+it('should expand transactions section in sidebar', function () {
+    app()->instance('request', createMockRequest('/docs/user-guides/transactions/how-to-vote-unvote'));
+
+    $document = Document::find('0d6eaf5f0b12c40882e0a648eecec8e5');
+
+    expect(Blade::render($document->body))->not->toMatch(
+        '/<div\s+class="lg:ml-8"\s+x-data="{ open: true }"\s+:class="{ \'last:pb-4\': ! open }"\s+x-cloak\s+>\s+'.
+        '<button\s+type="button"\s+class="flex items-center justify-between w-full pl-5 lg:pl-0 pr-5 border-theme-secondary-300 group pt-4 mt-4 border-t"\s+'.
+        '@click.prevent="open = ! open"\s+>\s+'.
+        '<h2 class="mb-0 text-base font-semibold text-left accordion-heading text-theme-secondary-900 group-hover:text-theme-primary-600">\s+Getting Started/'
+    );
+
+    expect(Blade::render($document->body))->toMatch(
+        '/<div\s+class="lg:ml-8"\s+x-data="{ open: true }"\s+:class="{ \'last:pb-4\': ! open }"\s+x-cloak\s+>\s+'.
+        '<button\s+type="button"\s+class="flex items-center justify-between w-full pl-5 lg:pl-0 pr-5 border-theme-secondary-300 group pt-4 mt-4 border-t"\s+'.
+        '@click.prevent="open = ! open"\s+>\s+'.
+        '<h2 class="mb-0 text-base font-semibold text-left accordion-heading text-theme-secondary-900 group-hover:text-theme-primary-600">\s+Transactions/'
     );
 });
