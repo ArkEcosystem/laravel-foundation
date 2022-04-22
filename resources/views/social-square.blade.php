d6ad990f773232cc7927b9404d005d097782152e
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2022-04-22 02:35:50 +0100

    additional customisation for social square component

--- resources/views/social-square.blade.php
+++ resources/views/social-square.blade.php
@@ -1,19 +1,30 @@
 @props([
     'icon',
     'url',
-    'internal' => false,
+    'internal'   => false,
+    'class'      => 'w-16 border h-14 border-theme-secondary-300 lg:w-14 lg:h-12 dark:border-theme-secondary-800',
     'hoverClass' => 'hover:bg-theme-danger-400 hover:text-white',
+    'iconSize'   => 'w-6 h-6 lg:w-5 lg:h-5',
 ])
 
 <a
-    href="{{ $url }}"
-    class="block w-16 border rounded-xl cursor-pointer h-14 border-theme-secondary-300 lg:w-14 lg:h-12 dark:border-theme-secondary-800 transition-default {{ $hoverClass }}"
+    {{ $attributes->merge([
+        'href'  => $url,
+        'class' => Arr::toCssClasses([
+            'block rounded-xl cursor-pointer transition-default',
+            $class,
+            $hoverClass,
+        ]),
+    ]) }}
     @unless($internal)
         target="_blank"
         rel="noopener noreferrer"
     @endunless
 >
     <div class="flex justify-center items-center h-full">
-        <x-ark-icon :name="$icon" size="w-6 h-6 lg:w-5 lg:h-5" />
+        <x-ark-icon
+            :name="$icon"
+            :size="$iconSize"
+        />
     </div>
 </a>
