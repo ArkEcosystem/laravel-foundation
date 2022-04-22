643b28c1d6609d6ec3336c709dea1f9ab79e8eee
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2022-04-22 16:01:56 +0100

    allow different chevron colour on open

--- resources/views/chevron-toggle.blade.php
+++ resources/views/chevron-toggle.blade.php
@@ -1,11 +1,15 @@
 @props([
     'isOpen',
     'size' => 'xs',
+    'openClass' => null,
 ])
 
 <span
     {{ $attributes->class('transition duration-150 ease-in-out') }}
-    :class="{ 'rotate-180': {{ $isOpen }} }"
+    :class="{ 'rotate-180 {{ Arr::toCssClasses([
+        'rotate-180',
+        $openClass
+    ]) }}': {{ $isOpen }} }"
 >
     <x-ark-icon name="arrows.chevron-down-small" :size="$size" />
 </span>
