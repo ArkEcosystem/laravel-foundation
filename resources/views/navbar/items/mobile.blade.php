383e6a39a24dbcbf10a22d01cf86a67dcb9c6ecb
Josip Crnković <jcrnkovic95@gmail.com>
2022-04-29 15:40:51 +0200

    pass attributes

--- resources/views/navbar/items/mobile.blade.php
+++ resources/views/navbar/items/mobile.blade.php
@@ -94,6 +94,7 @@
                     :name="$navItem['label']"
                     :params="$navItem['params'] ?? []"
                     :icon="isset($navItem['icon']) ? $navItem['icon'] : false"
+                    :attrs="$navItem['attributes'] ?? []"
                     :rounded="false"
                 />
             @endisset
