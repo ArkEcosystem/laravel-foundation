1cbe0b15515192e8c8253e0ae19a12daf940d123
ItsANameToo <ItsANameToo@users.noreply.github.com>
2022-05-18 10:11:08 +0000

    style: resolve style guide violations

--- resources/views/docs/sidebar-link.blade.php
+++ resources/views/docs/sidebar-link.blade.php
@@ -74,7 +74,7 @@
                     </span>
                 </div>
 
-                <div class="mt-1 mx-5 border-l border-theme-secondary-300">
+                <div class="mx-5 mt-1 border-l border-theme-secondary-300">
                     @foreach ($children as $child)
                         <div class="relative">
                             @php($childOnDocs = Request::onDocs($child['path']))
