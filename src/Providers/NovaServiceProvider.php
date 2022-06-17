cf0c1695a1c13e4032eae8b417e0b70f3777231f
Josip Crnković <crnkovic@users.noreply.github.com>
2022-06-17 14:31:20 +0200

    fix: disable permission tool from registering in Nova v4 (#384)

--- src/Providers/NovaServiceProvider.php
+++ src/Providers/NovaServiceProvider.php
@@ -103,9 +103,12 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     {
         self::resourcesIn(app_path('Nova'));
 
-        Nova::resources([
-            NovaPermission::class,
-            NovaRole::class,
-        ]);
+        // TODO: enable when https://github.com/vyuldashev/nova-permission merges in support for Nova v4...
+        if (! str_starts_with(Nova::version(), '4.')) {
+            Nova::resources([
+                NovaPermission::class,
+                NovaRole::class,
+            ]);
+        }
     }
 }
