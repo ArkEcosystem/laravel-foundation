9379116e4e1814a629c7c369d97e3246fdf54c70
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2022-07-25 18:18:42 +0100

    refactor: use postgres for testing

--- tests/Pest.php
+++ tests/Pest.php
@@ -5,5 +5,17 @@ declare(strict_types=1);
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use NunoMaduro\LaravelMojito\InteractsWithViews;
 use Tests\TestCase;
+use Tests\Blog\TestCase as BlogTestCase;
 
-uses(TestCase::class, InteractsWithViews::class, RefreshDatabase::class)->in(__DIR__);
+uses(TestCase::class, InteractsWithViews::class, RefreshDatabase::class)->in(
+    'Analysis',
+    'CommonMark',
+    'DataBags',
+    'Documentation',
+    'Fortify',
+    'NumberFormatter',
+    'Rules',
+    'Support',
+    'UserInterface',
+);
+uses(BlogTestCase::class, RefreshDatabase::class)->in('Blog');
