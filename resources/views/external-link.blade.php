297b9f9e8e6cddc09e91288afe42572fdd2bbb19
ItsANameToo <ItsANameToo@users.noreply.github.com>
2022-07-27 10:39:37 +0000

    style: resolve style guide violations

--- resources/views/external-link.blade.php
+++ resources/views/external-link.blade.php
@@ -18,7 +18,7 @@
         <x-ark-icon
             name="arrows.arrow-external"
             size="xs"
-            class="flex-shrink-0 inline relative ml-0.5 -top-1 -mt-0.5 text-theme-secondary-500"
+            class="inline relative -top-1 flex-shrink-0 ml-0.5 -mt-0.5 text-theme-secondary-500"
         />
     @endunless
 </a>
