8d067c6f85842585f0c351c6c9876c79a2820bb2
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2022-08-01 16:40:00 +0100

    article controller contract

---
+++ src/Blog/Controllers/Contracts/ArticleController.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ARKEcosystem\Foundation\DataBags\Contracts;
+
+use App\Models\Article;
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+
+interface ArticleController
+{
+    public function index(Request $request): Renderable;
+    public function show(Article $article): Renderable;
+}
