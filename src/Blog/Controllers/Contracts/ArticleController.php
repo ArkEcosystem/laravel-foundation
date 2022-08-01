af9d2dcbbb7910235f08827e2105c6e996d2bfbd
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2022-08-01 18:38:30 +0100

    wrong article model

--- src/Blog/Controllers/Contracts/ArticleController.php
+++ src/Blog/Controllers/Contracts/ArticleController.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace ARKEcosystem\Foundation\Blog\Controllers\Contracts;
 
-use App\Models\Article;
+use ARKEcosystem\Foundation\Blog\Models\Article;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 
