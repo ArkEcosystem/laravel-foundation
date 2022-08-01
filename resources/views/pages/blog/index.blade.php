02640e674325f5e46babf76e73f29986c6ba0fff
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2022-08-01 17:56:48 +0100

    update meta/title tags for blog home & author pages

--- resources/views/pages/blog/index.blade.php
+++ resources/views/pages/blog/index.blade.php
@@ -2,9 +2,10 @@
 
     <x-ark-metadata page="blog" />
 
-    @section('title', trans('metatags.home.title'))
+    @section('title', trans('metatags.blog.title'))
 
     @section('content')
         <livewire:blog-article-list :request="$request" />
     @endsection
+
 @endcomponent
