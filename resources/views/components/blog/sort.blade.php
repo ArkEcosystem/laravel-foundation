1f2efb93f0f305f883dbbdfaa526453bdd41d399
Alfonso Bribiesca <alfonsobries@gmail.com>
2022-08-01 04:00:35 -0500

    refactor: disable search & sorting if no author articles (#437)

--- resources/views/components/blog/sort.blade.php
+++ resources/views/components/blog/sort.blade.php
@@ -1,12 +1,22 @@
+@props([
+    'disabled' => false
+])
+
 <div class="flex space-x-2 text-sm font-medium">
     <span class="leading-none text-theme-secondary-900">
         @lang('general.sort_by'):
     </span>
 
     <div class="flex items-center divide-x divide-theme-primary-100">
-        <span 
-            class="flex items-center px-2 space-x-1 cursor-pointer" 
-            wire:click="sort"
+        <span
+            @class([
+                "flex items-center px-2 space-x-1",
+                "cursor-pointer" => !$disabled,
+                "text-theme-secondary-500" => $disabled,
+            ])
+            @unless ($disabled)
+                wire:click="sort"
+            @endunless
         >
             <span class="leading-none">
                 @lang('general.date')
