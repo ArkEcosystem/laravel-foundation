6e4943d55c0e303a11e803e84aa10b89fa755bbe
Josip Crnković <6536260+crnkovic@users.noreply.github.com>
2022-08-02 09:40:24 +0200

    Update sidebar-subheading.blade.php

--- resources/views/docs/sidebar-subheading.blade.php
+++ resources/views/docs/sidebar-subheading.blade.php
@@ -1,3 +1,3 @@
-<h3 {{ $attributes->class('pl-9 ml-8 lg:ml-5 mt-4 lg:pt-4 text-base font-semibold uppercase text-theme-secondary-500') }}>
+<h3 {{ $attributes->class('pl-9 ml-8 lg:ml-6 mt-4 lg:pt-4 pb-2 text-base font-semibold uppercase text-theme-secondary-500') }}>
     {{ $slot }}
 </h3>
