4e65d8f2072e681274cf8c9ea98872281262ee4d
Josip Crnković <6536260+crnkovic@users.noreply.github.com>
2022-08-02 13:02:33 +0200

    Update extended-footer.blade.php

--- resources/views/extended-footer.blade.php
+++ resources/views/extended-footer.blade.php
@@ -1,5 +1,5 @@
 <section {{ $attributes->class('bg-theme-secondary-900') }}>
-    <div class="flex flex-col py-12 px-10 mx-auto lg:flex-row lg:space-y-0 lg:space-x-6 lg:max-w-7xl lg:divide-x divide-theme-secondary-800">
+    <div class="flex flex-col py-12 px-8 md:px-10 mx-auto lg:flex-row lg:space-y-0 lg:space-x-6 lg:max-w-7xl lg:divide-x divide-theme-secondary-800">
         <div class="flex-1">
             <div class="space-y-4">
                 <h3 class="text-theme-secondary-200">@lang ('pages.extended-footer.help.title')</h3>
