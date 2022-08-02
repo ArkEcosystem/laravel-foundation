b302e82be2d94f38faf37cb3c2d61a3e7b48d237
Josip Crnković <6536260+crnkovic@users.noreply.github.com>
2022-08-02 12:49:58 +0200

    wip

--- resources/views/includes/footer/socials.blade.php
+++ resources/views/includes/footer/socials.blade.php
@@ -1,23 +1,6 @@
-<div {{ $attributes->class('space-y-4') }}>
-    <h3 class="text-theme-secondary-200">@lang('ui::pages.extended-footer.socials.title')</h3>
+<div class="{!! $class !!}">
+    <h3 class="text-theme-secondary-200">@lang ('pages.extended-footer.socials.title')</h3>
+    <p class="mt-4">@lang ('pages.extended-footer.socials.description')</p>
 
-    <div>@lang('ui::pages.extended-footer.socials.description')</div>
-
-    <div class="flex mt-6 space-x-2 text-theme-primary-600">
-        <x-ark-social-square
-            :url="trans('ui::urls.socials.github')"
-            icon="app-github"
-            class="border-2 border-theme-secondary-800 w-15 h-15"
-            hover-class="hover:text-white hover:bg-theme-primary-700 hover:border-theme-primary-700"
-            icon-size="w-8 h-8"
-        />
-
-        <x-ark-social-square
-            :url="trans('ui::urls.socials.twitter')"
-            icon="app-twitter"
-            class="border-2 border-theme-secondary-800 w-15 h-15"
-            hover-class="hover:text-white hover:bg-theme-primary-700 hover:border-theme-primary-700"
-            icon-size="w-8 h-8"
-        />
-    </div>
+    {{ $slot }}
 </div>
