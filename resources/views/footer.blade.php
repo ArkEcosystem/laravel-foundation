f0da61327bd5d7347a036a9c0c7c95551e4e4041
Josip Crnković <6536260+crnkovic@users.noreply.github.com>
2022-08-03 13:05:14 +0200

    Update footer.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -38,66 +38,64 @@
     ],
 ])
 
-<div {{ $attributes->class('border-t border-theme-secondary-800')->class($backgroundColor) }}>
-    <div class="{{ $wrapperClass }} mx-auto">
-        <div class="justify-between items-center py-5 space-y-4 lg:flex lg:space-y-0">
-            <div class="text-sm font-semibold leading-6 break-words">
-                <span>
-                    {{ date('Y') }}
-                    &copy;
-                    @if ($name && ! $url)
-                        {{ $name }}
-                    @elseif ($name && $url)
-                        <a href="{{ $url }}">{{ $name }}</a>
-                    @elseif (isset($creator['label']) || is_string($creator))
-                        <span>
-                            @lang ('ui::pages.footer.made_with_love') <a href="{{ $creator['url'] ?? url('/') }}" class="{{ $linkClass }}">{{ is_string($creator) ? $creator : $creator['label'] }}</a>
-                        </span>
-                    @endif
-                </span>
-
-                @if ((is_string($creator) || isset($creator['label'])) && $name)
-                    <span class="mx-0.5">|</span>
+<footer {{ $attributes->class('border-t border-theme-secondary-800')->class($backgroundColor) }}>
+    <div class="{{ $wrapperClass }} mx-auto justify-between items-center py-5 space-y-4 lg:flex lg:space-y-0">
+        <div class="text-sm font-semibold leading-6 break-words">
+            <span>
+                {{ date('Y') }}
+                &copy;
+                @if ($name && ! $url)
+                    {{ $name }}
+                @elseif ($name && $url)
+                    <a href="{{ $url }}" class="transition-default hover:text-theme-secondary-200">{{ $name }}</a>
+                @elseif (isset($creator['label']) || is_string($creator))
                     <span>
                         @lang ('ui::pages.footer.made_with_love') <a href="{{ $creator['url'] ?? url('/') }}" class="{{ $linkClass }}">{{ is_string($creator) ? $creator : $creator['label'] }}</a>
                     </span>
                 @endif
+            </span>
 
-                @if ($arkProduct)
-                    <span class="mx-0.5">|</span>
-                    <div>
-                        <x-ark-icon
-                            name="networks.ark-square"
-                            class="inline-block mr-1 -mt-1 ark-logo-red"
-                        />
+            @if ((is_string($creator) || isset($creator['label'])) && $name)
+                <span class="mx-0.5">|</span>
+                <span>
+                    @lang ('ui::pages.footer.made_with_love') <a href="{{ $creator['url'] ?? url('/') }}" class="{{ $linkClass }}">{{ is_string($creator) ? $creator : $creator['label'] }}</a>
+                </span>
+            @endif
 
-                        <span>An <a href="https://ark.io" class="{{ $linkClass }}">ARK.io</a> product</span>
-                    </div>
-                @endif
+            @if ($arkProduct)
+                <span class="mx-0.5">|</span>
+                <div>
+                    <x-ark-icon
+                        name="networks.ark-square"
+                        class="inline-block mr-1 -mt-1 ark-logo-red"
+                    />
 
-                @if ($reservedRights)
-                    <span class="mx-0.5">|</span>
-                    <span>@lang ('ui::pages.footer.rights')</span>
-                @endif
-
-                @if (isset($policy['url'], $policy['label']))
-                    <span class="mx-0.5">|</span>
-                    <a href="{{ $policy['url'] }}" class="{{ $linkClass }}">{{ $policy['label'] }}</a>
-                @endif
-
-                @if (isset($terms['url'], $terms['label']))
-                    <span class="mx-0.5">|</span>
-                    <a href="{{ $terms['url'] }}" class="{{ $linkClass }}">{{ $terms['label'] }}</a>
-                @endif
-            </div>
-
-            @if (count($socials) > 0)
-                <div class="flex space-x-3 leading-6">
-                    @foreach ($socials as $network)
-                        <x-ark-social-link :url="$network['url']" :icon="$network['icon']" data-safe-external />
-                    @endforeach
+                    <span>An <a href="https://ark.io" class="{{ $linkClass }}">ARK.io</a> product</span>
                 </div>
             @endif
+
+            @if ($reservedRights)
+                <span class="mx-0.5">|</span>
+                <span>@lang ('ui::pages.footer.rights')</span>
+            @endif
+
+            @if (isset($policy['url'], $policy['label']))
+                <span class="mx-0.5">|</span>
+                <a href="{{ $policy['url'] }}" class="{{ $linkClass }}">{{ $policy['label'] }}</a>
+            @endif
+
+            @if (isset($terms['url'], $terms['label']))
+                <span class="mx-0.5">|</span>
+                <a href="{{ $terms['url'] }}" class="{{ $linkClass }}">{{ $terms['label'] }}</a>
+            @endif
         </div>
+
+        @if (count($socials) > 0)
+            <nav class="flex space-x-3 leading-6">
+                @foreach ($socials as $network)
+                    <x-ark-social-link :url="$network['url']" :icon="$network['icon']" data-safe-external />
+                @endforeach
+            </nav>
+        @endif
     </div>
-</div>
+</footer>
