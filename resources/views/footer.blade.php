013aa81c4af66e246136b0d62ff4f1f3c1c7a61e
Josip Crnković <6536260+crnkovic@users.noreply.github.com>
2022-08-04 15:55:55 +0200

    add new window options

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,9 +2,9 @@
     'wrapperClass'  => 'px-8 max-w-7xl md:px-10',
     'backgroundColor' => 'bg-theme-secondary-900',
     'linkClass' => 'underline hover:no-underline transition-default hover:text-theme-secondary-200',
-    'creator' => [], // Needs `url` and `label` to display the "Made by"...
     'copyrightName' => null,
     'copyrightUrl' => null,
+    'creator' => [], // Needs `url` and `label` to display the "Made by"... Add `newWindow` to indicate whether to open the page in the new window or not...
     'isArkProduct' => false,
     'hasReservedRights' => true, // Show "All rights reserved"...
     'policy' => [], // Needs `url` and `label` to display the Privacy Policy link...
@@ -49,7 +49,15 @@
                     <a href="{{ $copyrightUrl }}" class="transition-default hover:text-theme-secondary-200">{{ $copyrightName }}</a>
                 @elseif (isset($creator['label']) || is_string($creator))
                     <span>
-                        @lang ('ui::pages.footer.made_with_love') <a href="{{ $creator['url'] ?? url('/') }}" class="{{ $linkClass }}" target="_blank" rel="noopener">{{ is_string($creator) ? $creator : $creator['label'] }}</a>
+                        @lang ('ui::pages.footer.made_with_love') <a
+                            href="{{ $creator['url'] ?? url('/') }}"
+                            class="{{ $linkClass }}"
+                            @if ($creator['newWindow'] ?? false)
+                            target="_blank" rel="noopener"
+                            @endif
+                        >
+                            {{ is_string($creator) ? $creator : $creator['label'] }}
+                        </a>
                     </span>
                 @endif
             </span>
@@ -57,7 +65,15 @@
             @if ((is_string($creator) || isset($creator['label'])) && $copyrightName)
                 <span class="mx-0.5">|</span>
                 <span>
-                    @lang ('ui::pages.footer.made_with_love') <a href="{{ $creator['url'] ?? url('/') }}" class="{{ $linkClass }}" target="_blank" rel="noopener">{{ is_string($creator) ? $creator : $creator['label'] }}</a>
+                    @lang ('ui::pages.footer.made_with_love') <a
+                        href="{{ $creator['url'] ?? url('/') }}" \
+                        class="{{ $linkClass }}"
+                        @if ($creator['newWindow'] ?? false)
+                        target="_blank" rel="noopener"
+                        @endif
+                    >
+                        {{ is_string($creator) ? $creator : $creator['label'] }}
+                    </a>
                 </span>
             @endif
 
