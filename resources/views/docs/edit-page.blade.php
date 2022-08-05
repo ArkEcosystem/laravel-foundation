b916d82c45f89d986cc50de204b6c999c5f378b1
ItsANameToo <itsanametoo@protonmail.com>
2022-08-05 12:47:15 +0200

    fix: center "edit page" on mobile for documentation pages

--- resources/views/docs/edit-page.blade.php
+++ resources/views/docs/edit-page.blade.php
@@ -6,11 +6,11 @@
     href="{{ config('web.github_source') }}/tree/master/{{ $document->type }}/{{ $document->slug }}.md.blade.php"
     target="_blank"
     rel="noopener nofollow noreferrer"
-    class="flex items-center pl-5 space-x-3 font-semibold link"
+    class="flex items-center sm:pl-5 space-x-3 font-semibold link"
 >
-    <x-ark-icon 
-        name="pencil" 
-        size="sm" 
+    <x-ark-icon
+        name="pencil"
+        size="sm"
     />
 
     <span>@lang('ui::actions.docs.edit_page')</span>
