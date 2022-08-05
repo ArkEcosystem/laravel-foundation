a754c92dc53bf581d08066a5c133e128144b736f
ItsANameToo <ItsANameToo@users.noreply.github.com>
2022-08-05 10:47:57 +0000

    style: resolve style guide violations

--- resources/views/docs/edit-page.blade.php
+++ resources/views/docs/edit-page.blade.php
@@ -6,7 +6,7 @@
     href="{{ config('web.github_source') }}/tree/master/{{ $document->type }}/{{ $document->slug }}.md.blade.php"
     target="_blank"
     rel="noopener nofollow noreferrer"
-    class="flex items-center sm:pl-5 space-x-3 font-semibold link"
+    class="flex items-center space-x-3 font-semibold sm:pl-5 link"
 >
     <x-ark-icon
         name="pencil"
