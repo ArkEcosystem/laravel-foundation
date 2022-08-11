0f0ca4895245962b8bbc918123ae94da4bd25ca8
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2022-08-11 14:11:52 +0100

    exception handler to change errors path

---
+++ src/UserInterface/Exceptions/Handler.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ARKEcosystem\Foundation\UserInterface\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\View;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * Register the error template hint paths.
+     *
+     * @return void
+     */
+    protected function registerErrorViewPaths()
+    {
+        View::replaceNamespace('errors', __DIR__.'/../../../resources/views/errors');
+    }
+}
