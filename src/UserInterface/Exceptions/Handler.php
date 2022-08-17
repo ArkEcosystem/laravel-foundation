d6797acfc646254398f8488a9df27af31a0220ce
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2022-08-17 11:01:29 +0100

    refactor: handle error codes outside templates

--- src/UserInterface/Exceptions/Handler.php
+++ src/UserInterface/Exceptions/Handler.php
@@ -6,6 +6,7 @@ namespace ARKEcosystem\Foundation\UserInterface\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Support\Facades\View;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class Handler extends ExceptionHandler
 {
@@ -17,5 +18,21 @@ class Handler extends ExceptionHandler
     protected function registerErrorViewPaths()
     {
         View::replaceNamespace('errors', __DIR__.'/../../../resources/views/errors');
+    }
+
+    /**
+     * Get the view used to render HTTP exceptions.
+     *
+     * @param  \Symfony\Component\HttpKernel\Exception\HttpExceptionInterface  $e
+     * @return string|null
+     */
+    protected function getHttpExceptionView(HttpExceptionInterface $e)
+    {
+        $view = parent::getHttpExceptionView($e);
+        if ($view) {
+            return $view;
+        }
+
+        return 'errors::500';
     }
 }
