f6ddb8dfd9711354a5191580e9b2b5e45e987cb7
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2022-08-17 12:30:05 +0100

    move view override to trait

---
+++ src/UserInterface/Exceptions/Concerns/OverridesExceptionView.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ARKEcosystem\Foundation\UserInterface\Exceptions\Concerns;
+
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+trait OverridesExceptionView
+{
+    /**
+     * Get the view used to render HTTP exceptions.
+     *
+     * @param  \Symfony\Component\HttpKernel\Exception\HttpExceptionInterface  $e
+     * @return string|null
+     */
+    protected function getHttpExceptionView(HttpExceptionInterface $e)
+    {
+        $view = parent::getHttpExceptionView($e);
+        if ($view) {
+            return $view;
+        }
+
+        return 'errors::500';
+    }
+}
--- src/UserInterface/Exceptions/Handler.php
+++ src/UserInterface/Exceptions/Handler.php
@@ -6,10 +6,11 @@ namespace ARKEcosystem\Foundation\UserInterface\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Support\Facades\View;
-use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class Handler extends ExceptionHandler
 {
+    use Concers\OverridesExceptionView;
+
     /**
      * Register the error template hint paths.
      *
@@ -18,21 +19,5 @@ class Handler extends ExceptionHandler
     protected function registerErrorViewPaths()
     {
         View::replaceNamespace('errors', __DIR__.'/../../../resources/views/errors');
-    }
-
-    /**
-     * Get the view used to render HTTP exceptions.
-     *
-     * @param  \Symfony\Component\HttpKernel\Exception\HttpExceptionInterface  $e
-     * @return string|null
-     */
-    protected function getHttpExceptionView(HttpExceptionInterface $e)
-    {
-        $view = parent::getHttpExceptionView($e);
-        if ($view) {
-            return $view;
-        }
-
-        return 'errors::500';
     }
 }
