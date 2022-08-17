7279eb9066a5b7612c5001e309d5882410febd34
alexbarnsley <alexbarnsley@users.noreply.github.com>
2022-08-17 11:32:21 +0000

    style: resolve style guide violations

--- tests/Stubs/ExceptionHandler.php
+++ tests/Stubs/ExceptionHandler.php
@@ -1,10 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Stubs;
 
 use ARKEcosystem\Foundation\UserInterface\Exceptions\Concerns\OverridesExceptionView;
 use Orchestra\Testbench\Exceptions\Handler as Base;
-use Throwable;
 
 class ExceptionHandler extends Base
 {
