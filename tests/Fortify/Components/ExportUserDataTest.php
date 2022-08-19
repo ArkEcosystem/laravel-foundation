3b42cfd0cd8d2d2b79edf117562dbf0d309a30c5
Josip Crnković <6536260+crnkovic@users.noreply.github.com>
2022-08-19 15:02:21 +0200

    Update ExportUserDataTest.php

--- tests/Fortify/Components/ExportUserDataTest.php
+++ tests/Fortify/Components/ExportUserDataTest.php
@@ -3,10 +3,10 @@
 declare(strict_types=1);
 
 use ARKEcosystem\Foundation\Fortify\Components\ExportUserData;
+use function Tests\createUserModel;
 use Illuminate\Support\Facades\Bus;
 use Livewire\Livewire;
 use Spatie\PersonalDataExport\Jobs\CreatePersonalDataExportJob;
-use function Tests\createUserModel;
 
 it('can export the user data', function () {
     Bus::fake();
@@ -28,8 +28,6 @@ it('can only export the user data once every 15 min', function () {
         ->assertEmitted('toastMessage', [trans('ui::pages.user-settings.data_exported'), 'success'])
         ->call('export')
         ->assertNotEmitted('toastMessage', [trans('ui::pages.user-settings.data_exported'), 'success']);
-
-    Bus::assertDispatched(CreatePersonalDataExportJob::class);
 
     $this->travel(16)->minutes();
 
