2efe91226c21fbf18e3a6f70811e72ced64bc22f
Josip Crnković <6536260+crnkovic@users.noreply.github.com>
2022-08-19 15:01:30 +0200

    wip

--- tests/UserInterface/Views/Inputs/InputWithPrefixTest.php
+++ tests/UserInterface/Views/Inputs/InputWithPrefixTest.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
+use function Tests\createAttributes;
 use Illuminate\Support\MessageBag;
 use Illuminate\Support\ViewErrorBag;
-use function Tests\createAttributes;
 
 it('should render with an icon', function (): void {
     $this
@@ -72,9 +72,9 @@ it('should render with the given model, but deferred', function (): void {
             'model'    => 'username_model',
             'deferred' => true,
         ]))
-        ->contains('type="text"')
-        ->contains('name="username"')
-        ->contains('wire:model.defer="username_model"');
+        ->assertSeeHtml('type="text"')
+        ->assertSeeHtml('name="username"')
+        ->assertSeeHtml('wire:model.defer="username_model"');
 });
 
 it('should render with the given placeholder', function (): void {
