efe3f129c914d6b8d674ab2a8abecc7954701532
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-01-12 22:46:28 +0000

    refactor: shape components

--- resources/views/shapes/square.blade.php
+++ resources/views/shapes/square.blade.php
@@ -1,13 +1,15 @@
 @props([
-    'colorClass'   => 'bg-theme-secondary-200 dark:bg-theme-secondary-900',
+    'colorClass'   => 'bg-theme-secondary-200 dark:bg-theme-secondary-800',
     'sizeClass'    => 'w-20 h-20',
-    'roundedClass' => 'rounded-xl',
+    'roundedClass' => 'rounded',
+    'pulse'        => false,
 ])
 
 <div>
-    <div @class([
+    <div {{ $attributes->class([
         $colorClass,
         $sizeClass,
         $roundedClass,
-    ])></div>
+        'animate-pulse' => $pulse,
+    ]) }}></div>
 </div>
