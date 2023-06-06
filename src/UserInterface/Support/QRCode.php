bb0ba93dd6d744bc193e12a0640577390f60152a
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-06-07 00:31:30 +0100

    refactor: allow dark mode colour overrides

--- src/UserInterface/Support/QRCode.php
+++ src/UserInterface/Support/QRCode.php
@@ -8,14 +8,19 @@ use BaconQrCode\Renderer\Image\SvgImageBackEnd;
 use BaconQrCode\Renderer\ImageRenderer;
 use BaconQrCode\Renderer\RendererStyle\RendererStyle;
 use BaconQrCode\Writer;
+use BaconQrCode\Renderer\RendererStyle\Fill;
 
 final class QRCode
 {
-    public static function generate(string $value, int $size = 250): string
+    public static function generate(string $value, int $size = 250, ?Fill $fill = null): string
     {
         $renderer = new ImageRenderer(
-            new RendererStyle($size, 1),
-            new SvgImageBackEnd()
+            new RendererStyle(
+                size: $size,
+                margin: 1,
+                fill: $fill,
+            ),
+            new SvgImageBackEnd(),
         );
 
         return (new Writer($renderer))->writeString($value);
