dae86bc900c7b0fd67c4fdd6b3ae4a72f7b78583
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2023-06-08 15:05:27 +0100

    refactor: clipboard checkmarks option (#571)

--- resources/views/clipboard.blade.php
+++ resources/views/clipboard.blade.php
@@ -1,20 +1,26 @@
 @props ([
     'value',
-    'class'          => 'h-10 w-12',
-    'copyInput'      => false,
-    'noStyling'      => false,
-    'tooltipContent' => trans('tooltips.copied'),
-    'wrapperClass'   => '',
+    'class'           => 'h-10 w-12',
+    'copyInput'       => false,
+    'noStyling'       => false,
+    'tooltipContent'  => trans('tooltips.copied'),
+    'wrapperClass'    => '',
+    'withCheckmarks'  => false,
+    'checkmarksClass' => null,
 ])
 
 <div
-    x-data="clipboard()"
+    x-data="clipboard({{ $withCheckmarks ? 'true' : 'false' }})"
     x-init="initClipboard()"
-    class="{{ $wrapperClass }}"
+    @class($wrapperClass)
 >
     <button
         type="button"
-        class="clipboard @unless($noStyling) button-icon @endif {{ $class }}"
+        @class([
+            'clipboard relative',
+            'button-icon' => ! $noStyling,
+            $class,
+        ])
         tooltip-content="{{ $tooltipContent }}"
         @if($copyInput)
             x-on:click="copyFromInput('{{ $value }}')"
@@ -22,6 +28,36 @@
             x-on:click="copy('{{ $value }}')"
         @endif
     >
-        <x-ark-icon name="copy" size="sm" />
+        @unless ($withCheckmarks)
+            <x-ark-icon
+                name="copy"
+                size="sm"
+            />
+        @else
+            <div
+                :class="{ 'opacity-0': showCheckmarks }"
+                class="transition-default"
+            >
+                <x-ark-icon
+                    name="copy"
+                    size="sm"
+                />
+            </div>
+
+            <div
+                x-show="showCheckmarks"
+                @class([
+                    'absolute m-auto',
+                    $checkmarksClass,
+                ])
+                x-cloak
+                x-transition
+            >
+                <x-ark-icon
+                    name="double-check-mark"
+                    size="sm"
+                />
+            </div>
+        @endif
     </button>
 </div>
