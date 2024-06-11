3d0123fe0629685d07ee486994fadb2c678c19be
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2024-06-11 14:35:06 +0100

    remove isolate for toasts

--- src/UserInterface/Components/Toast.php
+++ src/UserInterface/Components/Toast.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace ARKEcosystem\Foundation\UserInterface\Components;
 
-use Livewire\Attributes\Isolate;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
-#[Isolate]
 class Toast extends Component
 {
     public $toasts = [];
