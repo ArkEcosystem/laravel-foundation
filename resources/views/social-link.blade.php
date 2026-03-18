cc149de061c46644634759cf145a3712446f5bc8
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2026-03-18 17:00:26 +0000

    refactor: footer social link labels

--- resources/views/social-link.blade.php
+++ resources/views/social-link.blade.php
@@ -1,9 +1,20 @@
+@props([
+    'url',
+    'icon',
+    'label' => null,
+])
+
 <a
     {{ $attributes }}
     href="{{ $url }}"
     target="_blank"
     rel="noopener noreferrer"
     class="rounded transition-default hover:text-theme-secondary-200"
+    @if ($label) aria-label="{{ $label }}" @endif
 >
-    <x-ark-icon :name="$icon" size="sm" />
+    <x-ark-icon :name="$icon" size="sm" aria-hidden="true" />
+
+    @if ($label)
+        <span class="sr-only">{{ $label }}</span>
+    @endif
 </a>
